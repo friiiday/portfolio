1656fd0a78acd6156962ea41f719af5a1ad5aa47
friiiday <mayr.stefan@protonmail.com>
2026-05-23 15:50:50 +0200

    font styling

--- src/areas/Welcome.tsx
+++ src/areas/Welcome.tsx
@@ -1,20 +1,30 @@
 import "./welcome.css"
 import { CoverLetter } from "../components/coverLetter/CoverLetter"
-
 import { ScrambledText } from "../components/ScrambledText";
 
 export const Welcome = () => {
     
-
-    
-
     return (
         <div className="welcome" id="home-area" >
-            <ScrambledText 
-                text={ "Full-Stack Software Engineer Clean-Code Enthusiast Designer" } 
-                speed={ 0.2 } 
-                duration={ 3 } 
-            />
+            <div className="roles">
+                <ScrambledText 
+                    text={ "Full-Stack Software Engineer" } 
+                    speed={ 0.2 } 
+                    duration={ 3 } 
+                />
+                <br />
+                <ScrambledText 
+                    text={ "Software Architect" } 
+                    speed={ 0.3 } 
+                    duration={ 6 } 
+                />
+                <br />
+                <ScrambledText 
+                    text={ "Clean-Code Enthusiast" } 
+                    speed={ 0.4 } 
+                    duration={ 8 } 
+                />
+            </div>
             <CoverLetter />
         </div>
     )
